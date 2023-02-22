bec0e6cbab0ee47a2eb453e2e15875129d5ce1e1
Luke Miller <lam59@buffalo.edu>
2023-02-22 12:44:10 -0800

    Added outlined functionality w/ overflow hidden parallax effect.

--- client/src/pages/home/Landing.tsx
+++ client/src/pages/home/Landing.tsx
@@ -1,6 +1,7 @@
 // React
-import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useEffect, useState, useRef } from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 // Styles:
 import './landing.css';
@@ -14,29 +15,53 @@ import {easeInOut, motion} from 'framer-motion';
 const Landing = () => {
 
   let navigate = useNavigate();
+  let location = useLocation();
 
-  function newGame() {
-    navigate(`/create`);
-  }
+  // Handles browser changes (URL manually typed in & forward/backward)
+  useEffect(() => {
+    if(location.hash === '#create') {
+      setPageSection(true);
+      location.hash.replace("#create", "#home");
+    }
+    else {
+      setPageSection(false);
+    }
+  }, [location])
 
-  const spring = {
+  const [pageSection, setPageSection] = useState(false); // Sets if the user is on the landing page or the create game section
+
+  // Sets the type of animation Framer Motion will apply to the motion div
+  const tween = {
     type: "tween",
     duration: .75,
     ease: easeInOut,
   }
 
-  // transition={spring} initial={{backgroundSize: '150% 150%'}} animate={{backgroundSize: '100% 100%'}} exit={{backgroundSize: '150% 150%'}}
+  // Sets the different types of styles that the transition will occur over
+  const variants = {
+    open: {backgroundSize: 'auto 100%'},
+    closed: {backgroundSize: 'auto 150%'},
+  }
   
   return (
-    // <div className='bg__container'>
-      <motion.div className='bg__image' >
-        <div className='landing__center'>
+    <div className='bg__container'>
+      <motion.div className='bg__image' transition={tween} animate={pageSection ? "open" : "closed"} variants={variants}>
+        <div className='landing__center' id='home'>
           <h1>Geotrainer</h1>
           <p>A fun way to learn country flags, capitals, and more 🌏</p>
-          <Button variant="primary" size='lg' onClick={newGame}>Play Now</Button>
+          <HashLink smooth to='/#create'>
+            <Button variant="primary" size='lg'>Play Now</Button>
+          </HashLink>
+        </div>
+        <div className='landing__center' id='create'>
+          <h1>Choose Your Game</h1>
+          <p>Select the type of game you'd like to play, or</p>
+          <HashLink smooth to='/#home'>
+            <Button variant="primary" size='lg'>Back to top</Button>
+          </HashLink>
         </div>
       </motion.div>
-    // </div>
+    </div>
   )
 }
 
