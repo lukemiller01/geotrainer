eec94ad3aaa18e2ec569f8be40eefecd34505bff
Luke Miller <lam59@buffalo.edu>
2023-02-24 11:44:10 -0800

    Changing App.tsx to rafc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,3 @@
-// React
-// import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
 // Styles
 import './bootstrap.scss'; // Importing root stylesheet + custom SCSS
 import './app.css'; // Importing default HTML styles
@@ -9,8 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
  
-function App() {
-
+const App = () => {
   return (
     <>
     <BrowserRouter>
